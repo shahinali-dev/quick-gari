24512aca969bae1c74270b249c4392c2ad846492
shahinali-dev <shahinali.dev@gmail.com>
2025-10-09 10:50:18 +0600

    feat: migate services from function based to  class based

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -9,50 +9,49 @@ import { ISignIn } from "../user/user.interface";
 import UserModel from "../user/user.model";
 import { userService } from "../user/user.service";
 
-const signIn = async (payload: ISignIn) => {
-  const existingUser = await userService.isExist(payload.email);
-  if (!existingUser) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Invalid email");
+export class AuthService {
+  async signIn(payload: ISignIn) {
+    const existingUser = await userService.isExist(payload.email);
+    if (!existingUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Invalid email");
+    }
+
+    const isMatch = await passwordUtils.compare(
+      payload.password as string,
+      existingUser.password as string
+    );
+
+    if (!isMatch) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Invalid email or password");
+    }
+
+    const { password, ...rest } = existingUser.toJSON();
+
+    const jwtPayload = {
+      _id: rest._id,
+      email: rest.email,
+      role: rest.role,
+    };
+
+    const accessToken = createToken(
+      jwtPayload,
+      config.JWT_ACCESS_SECRET,
+      config.JWT_ACCESS_EXPIRE_IN
+    );
+
+    const refreshToken = createToken(
+      jwtPayload,
+      config.JWT_REFRESH_SECRET,
+      config.JWT_REFRESH_EXPIRE_IN
+    );
+
+    return { user: rest, accessToken, refreshToken };
   }
 
-  const isMatch = await passwordUtils.compare(
-    payload.password as string,
-    existingUser.password as string
-  );
-
-  if (!isMatch) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Invalid email or password");
+  async getAuthUser(id: string) {
+    const user = await UserModel.findById(id).select("-password");
+    return user;
   }
+}
 
-  const { password, ...rest } = existingUser.toJSON();
-
-  const jwtPayload = {
-    _id: rest._id,
-    email: rest.email,
-    role: rest.role,
-  };
-
-  const accessToken = createToken(
-    jwtPayload,
-    config.JWT_ACCESS_SECRET,
-    config.JWT_ACCESS_EXPIRE_IN
-  );
-
-  const refreshToken = createToken(
-    jwtPayload,
-    config.JWT_REFRESH_SECRET,
-    config.JWT_REFRESH_EXPIRE_IN
-  );
-
-  return { user: rest, accessToken, refreshToken };
-};
-
-const getAuthUser = async (id: string) => {
-  const user = await UserModel.findById(id).select("-password");
-  return user;
-};
-
-export const authService = {
-  signIn,
-  getAuthUser,
-};
+export const authService = new AuthService();
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -4,38 +4,37 @@ import passwordUtils from "../../utils/password_utils";
 import { IUser } from "./user.interface";
 import UserModel from "./user.model";
 
-const isExist = async (email: string) => {
-  const existingUser = await UserModel.findOne({ email });
-  return existingUser;
-};
-
-const registerUser = async (user: IUser) => {
-  const existingUser = await this.isExist(user.email);
-  if (existingUser) {
-    throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
+export class UserService {
+  async isExist(email: string) {
+    const existingUser = await UserModel.findOne({ email });
+    return existingUser;
   }
 
-  const { password, ...rest } = user;
-  if (!password) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Password is required");
+  async registerUser(user: IUser) {
+    const existingUser = await this.isExist(user.email);
+    if (existingUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, "User already exist");
+    }
+
+    const { password, ...rest } = user;
+    if (!password) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Password is required");
+    }
+
+    const hashedPassword = await passwordUtils.hash(password);
+    const newUser = await UserModel.create({
+      ...rest,
+      password: hashedPassword,
+    });
+
+    return await UserModel.findById(newUser._id).select("-password");
   }
 
-  const hashedPassword = await passwordUtils.hash(password);
-  const newUser = await UserModel.create({
-    ...rest,
-    password: hashedPassword,
-  });
+  async getAllUsers() {
+    const users = await UserModel.find().select("-password");
+    return users;
+  }
+}
 
-  return await UserModel.findById(newUser._id).select("-password");
-};
-
-const getAllUsers = async () => {
-  const users = await UserModel.find().select("-password");
-  return users;
-};
-
-export const userService = {
-  isExist,
-  registerUser,
-  getAllUsers,
-};
+// Export singleton instance
+export const userService = new UserService();
