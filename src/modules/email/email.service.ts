fdfc9b7294fff93b31b3fc4a90b9fb4eea1b2e04
shahinali-dev <shahinali.dev@gmail.com>
2026-02-22 12:58:59 +0600

    refactor: smtp config

--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -8,11 +8,16 @@ export class EmailService {
   private static getTransporter(): Transporter {
     if (!this.transporter) {
       this.transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
           user: config.APP_EMAIL,
           pass: config.APP_PASSWORD,
         },
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 15000,
       });
     }
     return this.transporter;
