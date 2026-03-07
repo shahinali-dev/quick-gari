902a368d2edcb62936a7c73d8216db0dbfa4a140
shahinali-dev <shahinali.dev@gmail.com>
2026-03-07 07:09:25 +0600

    feat: remove /api prefix from route

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response) => {
   res.send("Welcome to QuickGari API!");
 });
 
